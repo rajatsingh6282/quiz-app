0a2fe8cf072e986eb53039142c6fff5c3843d207
Rajat Singh <34193645+rajatsingh6282@users.noreply.github.com>
2021-10-07 17:05:30 +0530

    Update game.js
    update url

--- game.js
+++ game.js
@@ -66,7 +66,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
        localStorage.setItem("mostRecentScore", score);
        
-        return window.location.assign("/end.html");
+        return window.location.assign("/quiz-app/end.html");
     }
      questionCounter++;
      //questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`;
