278d281c2d266e42fee3e1c6d9db42100e53088e
Rajat Singh <34193645+rajatsingh6282@users.noreply.github.com>
2021-10-07 17:08:22 +0530

    Update end.js
    update refrence

--- end.js
+++ end.js
@@ -27,6 +27,6 @@ saveHighScore = e =>{
    highScores.splice(5);
 
    localStorage.setItem("highScores", JSON.stringify(highScores));
-   window.location.assign("/");
+   window.location.assign("/quiz-app");
     console.log(highScores);
 };
